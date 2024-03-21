6d3e114b1756a5875823a15aa7157dfb3b3e9f44
Yunus Yeşilçimen <135322317+esbyunus@users.noreply.github.com>
2024-03-22 02:23:58 +0300

    mini project added

---
+++ MiniProjects/PasswordCheck/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PassswordCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter password: ");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("Enter password again: ");
+            string passwordA =  Console.ReadLine() ;
+
+            if ( !password.Equals(string.Empty) && !password.Equals(string.Empty))
+            {
+                if (password.Equals(passwordA))
+                {
+                    Console.WriteLine("Passwords match! ");
+                }
+                else
+                {
+                    Console.WriteLine("Passwords dont match!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter a password please!");
+            }
+        }
+    }
+}
