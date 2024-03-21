f4c804b6aa8265e665411d1046cfb512f164b583
Yunus Yeşilçimen <135322317+esbyunus@users.noreply.github.com>
2024-03-21 21:41:33 +0300

    Update Program.cs
    Added description for tryparse

--- TryParse/Program.cs
+++ TryParse/Program.cs
@@ -4,6 +4,15 @@
     {
         static void Main(string[] args)
         {
+            /* --- Description ---
+            We're creating a boolean variable. We'll use the 'success' boolean in a while loop. 
+            We'll prompt the user for a 'numInput' input. We'll prevent using string instead of 
+            integer with an 'if'. If the 'numInput' defined as a string at the beginning of the 
+            loop receives input from the user as a string, the loop continues as 'true', and the 'else' part executes. 
+            If it receives input as an integer, the 'if' part executes, and the bool becomes 'false'. 
+            With TryParse, we can convert the integer input to an integer data without encountering an error. 
+            */
+            
             bool success = true;
 
             while (success)
