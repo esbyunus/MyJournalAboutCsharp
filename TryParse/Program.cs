7f0fe0b2326622ced77e0c50e290617f67c07388
Yunus Yeşilçimen <135322317+esbyunus@users.noreply.github.com>
2024-03-21 21:30:40 +0300

    Add files via upload

---
+++ TryParse/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool success = true;
+
+            while (success)
+            {
+                Console.WriteLine("Enter a number: ");
+                string numInput = Console.ReadLine();
+
+                if (int.TryParse(numInput, out int num))
+                {
+                    success = false;
+                    Console.WriteLine(num);
+                }
+                else
+                {
+                    Console.WriteLine("Failed to convert! ");
+                }
+            }
+        }
+    }
+}
